12ec84f411290684c917e1e73a9d37d60ed2f8b8
Delcio Capolo <delciocapolo096@gmail.com>
2023-03-11 21:11:22 +0100

    First Commit

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import debug from 'debug';
+
+class Main {
+	constructor() {
+		this.app = express();
+		this.log = debug('api:main');
+		this.port = 4002;
+	}
+	config() {
+		//As configuracoes
+		this.app.use(express.json());
+		this.app.use(express.urlencoded({ extended: true }));
+		this.app.use(express.static('public'));
+		this.app.set('view engine', 'ejs');
+	}
+	routes() {
+		//A gerencia das rotas todas estao aqui
+		this.app.get('/', (req, res) => {
+			res.render('index.ejs');
+		});
+	}
+	inicializador() {
+		this.config();
+		this.routes();
+		this.app.listen(this.port, () => this.log('Server is running'));
+	}
+}
+
+const Instancia1 = new Main();
+Instancia1.inicializador();
